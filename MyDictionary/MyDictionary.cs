973da8f17d861d787c3c854b1f22722eeaad3341
Büşra Yıldırım <busra_yldrrm@hotmail.com.tr>
2021-01-23 17:52:28 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TValue,TKey>
+    {
+        TValue[] item1;
+        TKey[] item2;
+    public MyDictionary()
+        {
+            item1 = new TValue[0];
+            item2 = new TKey[0];
+
+        }
+        public void Add(TValue items1, TKey items2)
+        {
+            TValue[] tempItem1 = item1;
+            TKey[] tempItem2 = item2;
+
+            item1 = new TValue[item1.Length + 1];
+            item2 = new TKey[item2.Length + 1];
+
+
+            for (int i = 0; i < tempItem1.Length; i++)
+            {
+                item1[i] = tempItem1[i];
+                item2[i] = tempItem2[i];
+
+
+
+
+            }
+
+            item1[item1.Length - 1] = items1;
+            item2[item2.Length - 1] = items2;
+
+
+        }
+
+
+        public void GetExam()
+        {
+
+            for (int i = 0; i < item1.Length; i++)
+            {
+                Console.WriteLine(item1[i] + " " + item2[i]);
+            }
+
+        }
+             
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> Exam = new MyDictionary<string, int>();
+            Exam.Add("Tarih neti:", 22);
+            Exam.Add("Vatandaşlık neti:", 12);
+            Exam.Add("Matematik neti:", 28);
+
+            Exam.GetExam();
+        }
+    }
+}
